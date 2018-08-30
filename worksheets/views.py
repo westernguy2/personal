9126b12c16b4b6357ce098c56b38b01e3c4bc848
Kunal Agarwal <kagarwal2@berkeley.edu>
2018-08-30 01:56:12 -0700

    Implementing 61A backend

--- worksheets/views.py
+++ worksheets/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 import operator
+from .models import Worksheet
 
 # Create your views here.
 def cs61a(request):
-	return render(request, 'worksheets/61a.html')
+	topic = request.GET['topic']
+	all_worksheets = Worksheet.objects
+	relevant_worksheets = []
+	for w in all_worksheets.all:
+		if getattr(w, topic) and not w.isSolution:
+			with_solution = []
+			for i in all_worksheets.all:
+				if i.worksheetNumber == w.worksheetNumber:
+					with_solution.append(i)
+			with_solution.sort(key = lambda x: x.isSolution)
+		relevant_worksheets.append(with_solution)
+
+	return render(request, 'worksheets/61a.html', {'wkshts':relevant_worksheets})
 
 def which_worksheets(request):
-	#topic = request.GET['topic']
 	return render(request, 'worksheets/pick_topic.html')
